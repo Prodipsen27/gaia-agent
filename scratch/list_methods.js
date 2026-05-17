8219775b316f80c02612a6353912989ce7d081d4
Prodip Sengupta <122111565+Prodipsen27@users.noreply.github.com>
2026-05-18 02:39:23 +0530

    Delete scratch directory

--- scratch/list_methods.js
+++
@@ -1,4 +0,0 @@
-import Firecrawl from "@mendable/firecrawl-js";
-const app = new Firecrawl({ apiKey: "test" });
-console.log("Methods:", Object.keys(app).filter(k => typeof app[k] === "function"));
-console.log("Proto Methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(app)).filter(k => typeof app[k] === "function"));
